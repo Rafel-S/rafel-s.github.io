8ae6f95c9f6d45b0b5f880562c87a4d9929a19bd
Rafel-S <RafelSalem.RS@Gmail.com>
2020-07-12 15:09:23 -0500

    First Commit.

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+var popout = document.getElementById("popout");
+var closeButton = document.getElementById("close-popout");
+var image = document.getElementById("variable-image");
+
+function currentImage(source) {
+  popout.style.display = "block";
+  image.src =  source;
+}
+
+closeButton.onclick = function() {
+  popout.style.display = "none";
+}
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 50) {
+    header.className =
+  }
+}
